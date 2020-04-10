3f1185c5a00bef693556e2b2fef6b146a69ddaab
Evan Bloemer <evan@multingo.com>
2020-04-10 12:59:43 +0900

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(
+      MaterialApp(
+        home: null,
+      ),
+    );
+
+class MagicEightBall extends StatefulWidget {
+  @override
+  _MagicEightBallState createState() => _MagicEightBallState();
+}
+
+class _MagicEightBallState extends State<MagicEightBall> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
